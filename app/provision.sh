098257b929fae8612ba2aa22c64284909a9e9abc
Armaanjasim <58048776+Armaanjasim@users.noreply.github.com>
2022-02-15 16:18:49 +0000

    Added Provision File

---
+++ app/provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+sudo apt-get install nodejs -y
+sudo apt-get install python-software-properties -y
+curl -sl https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+sudo apt-get install npm
+sudo npm install pm2 -g
+echo "export DB_HOST=mongodb://10.0.3.236:27017/posts" >> /home/vagrant/.bashrc
+source /home/vagrant/.bashrc
+sudo systemctl restart nginx
+sudo systemctl enable nginx
